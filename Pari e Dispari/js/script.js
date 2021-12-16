1d17eac80e063617b1de3195a86719bf62ac7e1e
GalluFrancesco <latuamaildigithub@email.comfrancesco.gallu@gmail.com>
2021-12-16 15:35:29 +0100

    Add: funzione per  num random

--- Pari e Dispari/js/script.js
+++ Pari e Dispari/js/script.js
@@ -1,3 +1,9 @@
+//funzione che genera un numero random fra max e min
+//e lo restituisce
+function numeroRandom(numMin, numMax){
+    let risultato= Math.floor(Math.random() * ((numMax + 1) - numMin)) + numMin;
+    return risultato;
+}
 
 
 
